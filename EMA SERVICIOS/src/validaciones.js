608539e80e7dd15900f8cc7a10401b4e60e955cd
rydsistemas2023-dev <rydsistemas.2023@gmail.com>
2026-06-03 11:07:20 -0300

    Update validaciones.js

--- EMA SERVICIOS/src/validaciones.js
+++ EMA SERVICIOS/src/validaciones.js
@@ -1,1 +1,32 @@
+function esNumero(valor) {
+  return /^\d+$/.test(valor);
+}
 
+function detectarDistribuidora(texto) {
+
+  texto = texto.toLowerCase();
+
+  if (
+    texto.includes("1") ||
+    texto.includes("edenor") ||
+    texto.includes("edeno") ||
+    texto.includes("eden")
+  ) {
+    return "EDENOR";
+  }
+
+  if (
+    texto.includes("2") ||
+    texto.includes("naturgy") ||
+    texto.includes("natur")
+  ) {
+    return "NATURGY";
+  }
+
+  return null;
+}
+
+module.exports = {
+  esNumero,
+  detectarDistribuidora
+};
